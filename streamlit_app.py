5d1968d9361d00f5139bc3eea0b32604d3e013b2
arypr <123067276+arypr@users.noreply.github.com>
2023-08-07 12:37:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import pickle
 
-df = pd.read_excel ('https://raw.githubusercontent.com/arypr/Prediksi-Harga-Rumah-Tebet-app/main/data_bersih%20(1)xlsx')
+df = pd.read_excel ('https://github.com/arypr/Prediksi-Harga-Rumah-Tebet-app/blob/main/data_bersih%20(1).xlsx')
 
 model = LinearRegression()
 
