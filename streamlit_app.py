3c30d9588b5ad48f4dca3450a356755a26deb88d
arypr <123067276+arypr@users.noreply.github.com>
2023-08-20 18:02:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
 # Load Dataset
-df = pd.read_excel("https://raw.githubusercontent.com/arypr/Prediksi-Harga-Rumah-Tebet-app/main/df_prediksi.xlsx
-", engine="openpyxl")
+df = pd.read_excel('https://raw.githubusercontent.com/arypr/Prediksi-Harga-Rumah-Tebet-app/main/df_prediksi.xlsx', engine='openpyxl')
 
 X= df[['LB', 'LT', 'KT', 'KM', 'GRS']]
 y = df['HARGA']
